eb778d1bc97609b863894999b7f9973e4091da75
Gabriel Trujillo <joaqt23@gmail.com>
2022-05-18 13:39:27 -0500

    Basic promise

--- btc.js
+++ btc.js
@@ -2,13 +2,24 @@ const fs = require('fs');
 const path = require('path');
 
 function processFile(fileName) {
-    if (!fileName) throw new Error('No file provided');
+    return new Promise((resolve, reject) => {
+        if (!fileName) reject(new Error('No file provided'));
 
-    const filePath = path.join(__dirname, fileName);
-    setTimeout(() => console.log(`Completed: ${filePath}`), 2000);
+        const filePath = path.join(__dirname, fileName);
+            setTimeout(() => resolve(filePath), 2000);
+        });
 }
 
-const result = processFile('btc.csv')
-console.log(`Expected: ${result}`);
+const result = processFile()
+    .then(successHandler)
+    .catch(rejectHandler)
+
+function successHandler(result) {
+    console.log(`From promise: ${result}`);
+}
+
+function rejectHandler(err) {
+    console.log(err.message);
+}
 
 
