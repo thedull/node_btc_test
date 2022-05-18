a3aecfb6b7be7242320aa903ff00c1a352675c98
Gabriel Trujillo <joaqt23@gmail.com>
2022-05-18 18:07:25 -0500

    Added basic file processing

--- btc.js
+++ btc.js
@@ -4,17 +4,31 @@ const path = require('path');
 async function processFile(fileName) {
     return new Promise((resolve, reject) => {
         if (!fileName) reject(new Error('No file provided'));
-
+        
         const filePath = path.join(__dirname, fileName);
-            setTimeout(() => resolve(filePath), 2000);
-        });
+        let fileContents = '';
+
+        const onDataHandler = chunk => {
+            console.log(`Received chunk: ${chunk.length}`);
+            fileContents += chunk.toString();
+        };
+
+        const onEndHandler = () => resolve(fileContents);
+
+        const onErrorHandler = error => reject(error);
+
+        fs.createReadStream(filePath)
+            .on('data', onDataHandler)
+            .on('end', onEndHandler)
+            .on('error', onErrorHandler)
+    });
 }
 
 (async function main() {
     try {
-        const result = await processFile('btc.js');
-        console.log(`From promise: ${result}`);   
+        const result = await processFile('btc.csv');
+        console.log(result.slice(0,200));
     } catch (error) {
-        console.log(err.message);    
+        console.log(error.message);    
     }
 })()
