d149fc84a0a50d7931cbd36c2c42d2a19b20ecdb
Gabriel Trujillo <joaqt23@gmail.com>
2022-05-18 19:25:38 -0500

    Data filtering by date range

--- btc.js
+++ btc.js
@@ -27,9 +27,14 @@ async function processFile(fileName) {
 async function processData(dataBuffer) {
     return new Promise((resolve, reject) => {
         try {
-            const startLine = +process.argv[2] || 1;
-            const endLine = +process.argv[3] || Infinity;
-            const dataArr = dataBuffer.split("\r\n").slice(startLine-1, endLine).join("\r\n");
+            const dataArr = dataBuffer.split("\r\n").slice(1)
+                .map(line => {
+                    const entries = line.split(',\"');
+                    const date = new Date(entries[0].replace('\"', ''));
+                    const price = +(entries[1].replace('\"', '').replace(',', '')); 
+                    return { date, price };
+                })
+                .sort((a, b) => a.date.getTime() - b.date.getTime());
             resolve(dataArr);   
         } catch (error) {
             reject(error.message);
@@ -37,11 +42,29 @@ async function processData(dataBuffer) {
     });
 }
 
+function getRange(rawData, startDate, endDate) {
+    if (startDate > endDate) throw new Error('Start date after end date');
+
+    let startIndex = rawData.findIndex(entry => entry.date.toISOString().slice(0,10) === startDate.toISOString().slice(0,10));
+    if (startIndex === -1) startIndex = 0; 
+    let endIndex = rawData.findIndex(entry => entry.date.toISOString().slice(0,10) === endDate.toISOString().slice(0,10));
+    if (endIndex === -1) endIndex = rawData.length;
+    
+    return rawData.slice(startIndex, endIndex+1);
+} 
+
 (async function main() {
     try {
+        const startDate = new Date(process.argv[2]);
+        const endDate = new Date(process.argv[3]);
+        if (!startDate || !endDate) reject(new Error('Invalid arguments'));
+
         const result = await processFile('btc.csv');
-        console.log(await processData(result));
+        const rawData = await processData(result);
+        const data = getRange(rawData, startDate, endDate);
+        console.log({ data });
     } catch (error) {
         console.log(error.message);    
+        console.log('Usage: node btc.js <startDate (YYYY-MM-DD)> [endDate (YYYY-MM-DD)]');    
     }
 })()
